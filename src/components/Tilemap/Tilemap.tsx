fc77c3aadaf046a854307fd47a534a224d535c65
Carlos de la Cuadra Corada <carlos.delacuadra@devo.com>
2021-09-30 12:44:03 +0200

    chore: use dispatchOnChange Hook

--- src/components/Tilemap/Tilemap.tsx
+++ src/components/Tilemap/Tilemap.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useReducer, useEffect} from 'react';
+import React, { FunctionComponent, useReducer} from 'react';
 import { TileSchema } from '../..';
 import { setColSize, setDragSpeedRatio, setRowSize, setTileSchema } from './context/TilemapActions';
 import { TilemapContext } from './context/TilemapContext';
@@ -6,6 +6,7 @@ import { tilemapReducer } from './context/TilemapReducer';
 import { initialState } from './context/TilemapState';
 import Table from './components/Table';
 import './tilemap.scss';
+import { distpatchOnChange } from '../../hooks/dispatchOnChange';
 
 export type TilemapProps = {
   rowSize?: number;
@@ -17,29 +18,31 @@ export type TilemapProps = {
 const Tilemap: FunctionComponent<TilemapProps> = (props) => {
   const [state, dispatch] = useReducer(tilemapReducer, initialState);
 
-  useEffect(() => {
-    if(props.rowSize){
-      dispatch(setRowSize(props.rowSize));
-    }
-  }, [props.rowSize]);
+  // Props binding
+  
+  distpatchOnChange(
+    props.rowSize,
+    dispatch,
+    setRowSize,
+  );
 
-  useEffect(() => {
-    if(props.colSize){
-      dispatch(setColSize(props.colSize));
-    }
-  }, [props.colSize]);
+  distpatchOnChange(
+    props.colSize,
+    dispatch,
+    setColSize,
+  );
 
-  useEffect(() => {
-    if(props.dragSpeedRatio){
-      dispatch(setDragSpeedRatio(props.dragSpeedRatio));
-    }
-  }, [props.dragSpeedRatio]);
+  distpatchOnChange(
+    props.dragSpeedRatio,
+    dispatch,
+    setDragSpeedRatio,
+  );
 
-  useEffect(() => {
-    if(props.tileSchemas){
-      dispatch(setTileSchema(props.tileSchemas));
-    }
-  }, [props.tileSchemas]);
+  distpatchOnChange(
+    props.tileSchemas,
+    dispatch,
+    setTileSchema,
+  );
 
   return (
     <TilemapContext.Provider value={{state, dispatch}}>
